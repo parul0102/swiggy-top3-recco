b9193e203365ce480e9a8194bedf5247f642b516
Parul <jparul187@gmail.com>
2026-03-06 11:06:02 +0530

    cookicutter initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='it will help recommend the top3 food items on swiggy home page',
+    author='Parul',
+    license='',
+)
